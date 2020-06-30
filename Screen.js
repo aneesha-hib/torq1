ff04cabe8982439595de0017a87b048936b67423
Aneesha Jenson <aneeshajenson27@gmail.com>
2020-06-30 15:25:23 +0530

    screen update

--- Screen.js
+++ Screen.js
@@ -1,40 +1,58 @@
-import React, { PureComponent } from 'react';
-import { Text, Dimensions, Image, StyleSheet, View } from 'react-native';
+import React from 'react';
+import {
+  StyleSheet, View, Text, Image, Dimensions,
+} from 'react-native';
+import Carousel, { PaginationLight } from 'react-native-x-carousel';
 
-import Swiper from "react-native-web-swiper";
+const { width } = Dimensions.get('window');
 
-export const {width,height}=Dimensions.get('window');
+const DATA = [
+  {
+    
+    cornerLabelText: 'Advertisement',
+   
+  },
+  {
+   
+    cornerLabelText: 'Advertisement ',
+  },
+  {
+    
+    cornerLabelText: 'Advertisement ',
+  },
+  {
+   
+    cornerLabelText: 'Advertisement ',
+  },
+];
+
+const Screen = () => {
+  const renderItem = data => (
+   
+
+    <View style={styles.container1}>
+    <Text style={styles.textAdvertisement}>  { data.cornerLabelText }</Text>
+
+</View>
+  );
+
+  return (
+    <View style={styles.container}>
+      <Carousel
+        pagination={PaginationLight}
+        renderItem={renderItem}
+        data={DATA}
+        loop
+        autoplay
+      />
+    </View>
+  );
+};
+
 const styles = StyleSheet.create({
-    container: {
-        
-      height:width*.11,
-      width:width*.30,
-      justifyContent:'center',
-      borderTopLeftRadius: 20,
-      borderTopRightRadius: 20,
-      borderBottomLeftRadius: 20,
-      borderBottomRightRadius: 20, 
-     
-
-        
-    },
-    slideContainer: {
-        flex: 1,
-        alignItems: "center",
-        justifyContent: "center"
-    },
-    slide1: {
-        backgroundColor: "#dcdcdc"
-    },
-    slide2: {
-        backgroundColor: "#dcdcdc"
-    },
-    slide3: {
-        backgroundColor: "#dcdcdc"
-    },
-    container1: {
+  container: {
         height:width*.08,
-        width:width*-.30,
+        width:width*.30,
         backgroundColor: '#a1a9bc',
         justifyContent: 'center',
         alignItems:'center',
@@ -42,40 +60,52 @@ const styles = StyleSheet.create({
         borderTopRightRadius: width*.008,
         borderBottomLeftRadius:  width*.008,
         borderBottomRightRadius:  width*.008
-      },
-      textAdvertisement:
+     
+  },
+  cardContainer: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    width,
+  },
+  cardWrapper: {
+    borderRadius: 8,
+    overflow: 'hidden',
+  },
+  card: {
+    width: width * 0.9,
+    height: width * 0.5,
+  },
+  cornerLabel: {
+    position: 'absolute',
+    bottom: 0,
+    right: 0,
+    borderTopLeftRadius: 8,
+  },
+  cornerLabelText: {
+    fontSize: 12,
+    color: '#fff',
+    fontWeight: '600',
+    paddingLeft: 5,
+    paddingRight: 5,
+    paddingTop: 2,
+    paddingBottom: 2,
+  },
+  container1: {
+    height:width*.08,
+    width:width*-.30,
+    backgroundColor: '#a1a9bc',
+    justifyContent: 'center',
+    alignItems:'center',
+    borderTopLeftRadius: width*.008,
+    borderTopRightRadius: width*.008,
+    borderBottomLeftRadius:  width*.008,
+    borderBottomRightRadius:  width*.008
+  },
+  textAdvertisement:
   {
       color:'#354e76',
       fontSize:width*.01
   },
 });
 
-export default class Screen extends PureComponent {
-    render() {
-        return (
-            <View style={styles.container}>
-                <Swiper
-               
-                 from={1} 
-                 loop 
-                 timeout={3} 
-                 springConfig={{ speed: 8 }} 
-                //  minDistanceForAction={0.15}
-                >
-               
-                        
-
-               
-            <View style={styles.container1}>
-                        <Text style={styles.textAdvertisement}>Advertisement</Text>
-
-            </View>
-            <View style={styles.container1}>
-                        <Text style={styles.textAdvertisement}>Advertisement</Text>
-
-            </View>
-                </Swiper>
-            </View>
-        )
-    }
-}
+export default Screen;
